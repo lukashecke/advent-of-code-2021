0cac4820372d79ae72e3d71006d206132da53a5e
Lukas Hecke <Lukas.Hecke@meierhofer.com>
2021-12-09 22:04:01 +0100

    cleanup day 9 part 1

--- day09/part1.py
+++ day09/part1.py
@@ -1,54 +1,54 @@
 with open('input.txt') as file:
     input = file.read()
 
-map = [list(map(int, list(j))) for j in input.split('\n')]
+map = [list(map(int, list(line))) for line in input.split('\n')]
 
 risk_level = 0
 
-for j in range(100):
+for y in range(100):
     check = 0
-    for i in range(100):
-        if i > 0 and i < 99 and j > 0 and j < 99:  # all 'middle' numbers
-            if map[i][j] < map[i][j - 1]:  # left
-                if map[i][j] < map[i - 1][j]:  # top
-                    if map[i][j] < map[i][j + 1]:  # right
-                        if map[i][j] < map[i + 1][j]:  # bottom
-                            risk_level += map[i][j] + 1
-        if i == 0 and j > 0 and j < 99:  # all top 'middle' numbers
-            if map[i][j] < map[i][j - 1]:  # left
-                if map[i][j] < map[i][j + 1]:  # right
-                    if map[i][j] < map[i + 1][j]:  # bottom
-                        risk_level += map[i][j] + 1
-        if i > 0 and i < 99 and j == 0:  # all left 'middle' numbers
-            if map[i][j] < map[i - 1][j]:  # top
-                if map[i][j] < map[i][j + 1]:  # right
-                    if map[i][j] < map[i + 1][j]:  # bottom
-                        risk_level += map[i][j] + 1
-        if i > 0 and i < 99 and j == 99:  # all right 'middle' numbers
-            if map[i][j] < map[i - 1][j]:  # top
-                if map[i][j] < map[i][j - 1]:  # left
-                    if map[i][j] < map[i + 1][j]:  # bottom
-                        risk_level += map[i][j] + 1
-        if i == 99 and j > 0 and j < 99:  # all bottom 'middle' numbers
-            if map[i][j] < map[i][j - 1]:  # left
-                if map[i][j] < map[i - 1][j]:  # top
-                    if map[i][j] < map[i][j + 1]:  # right
-                        risk_level += map[i][j] + 1
-        if i == 0 and j == 0:  # top left number
-            if map[i][j] < map[i][j + 1]:  # right
-                if map[i][j] < map[i + 1][j]:  # bottom
-                    risk_level += map[i][j] + 1
-        if i == 0 and j == 99:  # top right number
-            if map[i][j] < map[i][j - 1]:  # left
-                if map[i][j] < map[i + 1][j]:  # bottom
-                    risk_level += map[i][j] + 1
-        if i == 99 and j == 0:  # bottom left number
-            if map[i][j] < map[i - 1][j]:  # top
-                if map[i][j] < map[i][j + 1]:  # right
-                    risk_level += map[i][j] + 1
-        if i == 99 and j == 99:  # bottom right number
-            if map[i][j] < map[i][j - 1]:  # left
-                if map[i][j] < map[i - 1][j]:  # top
-                    risk_level += map[i][j] + 1
+    for x in range(100):
+        if x > 0 and x < 99 and y > 0 and y < 99:  # all 'middle' numbers
+            if map[x][y] < map[x - 1][y]:  # left
+                if map[x][y] < map[x][y - 1]:  # top
+                    if map[x][y] < map[x + 1][y]:  # right
+                        if map[x][y] < map[x][y + 1]:  # bottom
+                            risk_level += map[x][y] + 1
+        elif x > 0 and x < 99 and y == 0:  # all top 'middle' numbers
+            if map[x][y] < map[x - 1][y]:  # left
+                if map[x][y] < map[x + 1][y]:  # right
+                    if map[x][y] < map[x][y + 1]:  # bottom
+                        risk_level += map[x][y] + 1
+        elif x == 0 and y > 0 and y < 99:  # all left 'middle' numbers
+            if map[x][y] < map[x][y - 1]:  # top
+                if map[x][y] < map[x + 1][y]:  # right
+                    if map[x][y] < map[x][y + 1]:  # bottom
+                        risk_level += map[x][y] + 1
+        elif x == 99 and y > 0 and y < 99:  # all right 'middle' numbers
+            if map[x][y] < map[x][y - 1]:  # top
+                if map[x][y] < map[x - 1][y]:  # left
+                    if map[x][y] < map[x][y + 1]:  # bottom
+                        risk_level += map[x][y] + 1
+        elif x > 0 and x < 99 and y == 99:  # all bottom 'middle' numbers
+            if map[x][y] < map[x - 1][y]:  # left
+                if map[x][y] < map[x][y - 1]:  # top
+                    if map[x][y] < map[x + 1][y]:  # right
+                        risk_level += map[x][y] + 1
+        elif x == 0 and y == 0:  # top left number
+            if map[x][y] < map[x + 1][y]:  # right
+                if map[x][y] < map[x][y + 1]:  # bottom
+                    risk_level += map[x][y] + 1
+        elif x == 99 and y == 0:  # top right number
+            if map[x][y] < map[x - 1][y]:  # left
+                if map[x][y] < map[x][y + 1]:  # bottom
+                    risk_level += map[x][y] + 1
+        elif x == 0 and y == 99:  # bottom left number
+            if map[x][y] < map[x][y - 1]:  # top
+                if map[x][y] < map[x + 1][y]:  # right
+                    risk_level += map[x][y] + 1
+        elif x == 99 and y == 99:  # bottom right number
+            if map[x][y] < map[x - 1][y]:  # left
+                if map[x][y] < map[x][y - 1]:  # top
+                    risk_level += map[x][y] + 1
 
 print(risk_level)
