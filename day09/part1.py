8ee9c3b8b6ff5401e4d8d7e5cef14a64002abc14
Lukas Hecke <Lukas.Hecke@meierhofer.com>
2021-12-10 19:36:33 +0100

    cleanup day 9 part 1

--- day09/part1.py
+++ day09/part1.py
@@ -8,28 +8,28 @@ risk_level = 0
 for y in range(100):
     check = 0
     for x in range(100):
-        if x > 0 and x < 99 and y > 0 and y < 99:  # all 'middle' numbers
+        if 0 < x < 99 and 0 < y < 99:  # all 'middle' numbers
             if map[x][y] < map[x - 1][y]:  # left
                 if map[x][y] < map[x][y - 1]:  # top
                     if map[x][y] < map[x + 1][y]:  # right
                         if map[x][y] < map[x][y + 1]:  # bottom
                             risk_level += map[x][y] + 1
-        elif x > 0 and x < 99 and y == 0:  # all top 'middle' numbers
+        elif 0 < x < 99 and y == 0:  # all top 'middle' numbers
             if map[x][y] < map[x - 1][y]:  # left
                 if map[x][y] < map[x + 1][y]:  # right
                     if map[x][y] < map[x][y + 1]:  # bottom
                         risk_level += map[x][y] + 1
-        elif x == 0 and y > 0 and y < 99:  # all left 'middle' numbers
+        elif x == 0 and 0 < y < 99:  # all left 'middle' numbers
             if map[x][y] < map[x][y - 1]:  # top
                 if map[x][y] < map[x + 1][y]:  # right
                     if map[x][y] < map[x][y + 1]:  # bottom
                         risk_level += map[x][y] + 1
-        elif x == 99 and y > 0 and y < 99:  # all right 'middle' numbers
+        elif x == 99 and 0 < y < 99:  # all right 'middle' numbers
             if map[x][y] < map[x][y - 1]:  # top
                 if map[x][y] < map[x - 1][y]:  # left
                     if map[x][y] < map[x][y + 1]:  # bottom
                         risk_level += map[x][y] + 1
-        elif x > 0 and x < 99 and y == 99:  # all bottom 'middle' numbers
+        elif 0 < x < 99 and y == 99:  # all bottom 'middle' numbers
             if map[x][y] < map[x - 1][y]:  # left
                 if map[x][y] < map[x][y - 1]:  # top
                     if map[x][y] < map[x + 1][y]:  # right
